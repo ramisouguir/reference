46e834a969019aaf14f8928777e9bc820bd04827
Rami <rws2012mc@gmail.com>
2020-05-22 16:13:40 -0400

    First commit

---
+++ canvas.js
@@ -0,0 +1,16 @@
+const c = document.getElementById("canvas");
+const canvas = c.getContext();
+
+function makeCircle()
+{
+    canvas.beginPath();
+    canvas.arc(200, 200, 40, 0, 2 * Math.PI);
+    canvas.stroke();
+}
+
+function main()
+{
+    makeCircle();
+}
+
+main();
